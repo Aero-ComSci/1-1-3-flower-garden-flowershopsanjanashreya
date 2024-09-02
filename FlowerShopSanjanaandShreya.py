3f924b91e2c8e65051423ae1519ceacb390f224f
Sanjana Gowda <sanjanagowda@Sanjanas-MacBook-Air.local>
2024-09-02 12:12:12 -0700

    Starting

---
+++ FlowerShopSanjanaandShreya.py
@@ -0,0 +1,9 @@
+# Flower Shop intro
+print(" . . . . . . . . . . . . . . . . . . .\n*. Welcome to the Flawless FLower Shop! .*\n . . . . . . . . . . . . . . . . . . . \nWe create custom bouquets for every occasion!\n")
+
+# User order
+order = input("\n\nWhat would you like to order today?\n")
+
+# In Stock
+
+flowers = [rose, sunflower, daisy, poppy, tulip]
